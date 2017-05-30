93762422f6e38ecc9d6ef541f2d0bbc82fcd2726
Dani <danypr92@gmail.com>
2017-05-30 21:06:27 +0200

    Add verbose to ansible and refactor db setup

--- scripts/provisioning-dev.sh
+++ scripts/provisioning-dev.sh
@@ -15,31 +15,29 @@ playbook="playbooks/development.yml"
 source "$PWD/scripts/config/lxc.cfg"
 source "$PWD/scripts/config/ansible.cfg"
 # Check if container exist
-
 # Install python2.7 in container:
 echo "Installing Python2.7"
 sudo lxc-attach -n "$name" -- sudo apt update
 sudo lxc-attach -n "$name" -- sudo apt install -y python2.7
-
+echo
 # Install the community role dependencies of the playbooks
-echo "Installing community dependencies of playbooks"
+echo "Installing ansible community dependencies of playbooks"
 bin/setup
-
+echo
 # Execute playbook development.yml:
 echo "Ansible playbook"
-ansible-playbook "$playbook" -u "$app_user" -i "$inv" -e 'ansible_python_interpreter=/usr/bin/python2.7' --limit=lxc --ask-sudo-pass
+ansible-playbook "$playbook" -u "$app_user" -i "$inv" -e 'ansible_python_interpreter=/usr/bin/python2.7' -vvv --limit=lxc --ask-sudo-pass
 echo "Provision OK!"
 echo
-echo "Accessing to $host"
-ssh "$app_user"@"$host" -A <<- EOF
-        cd openfoodnetwork/
-        echo "Installing ruby application and gem dependencies"
-        bundle install
-        echo "Postgres ofn user created"
-        echo "Creating the databases usung the setting specified in config/database.yml and populate them..."
-        bundle exec rake db:setup
-        echo
-        echo "Load default data for development environment..."
-        bundle exec rake openfoodnetwork:dev:load_sample_data
+echo "Accessing to $host with user $app_user"
+ssh "$app_user"@"$host" << EOF
+  cd openfoodnetwork/
+  echo "Installing ruby application and gem dependencies"
+  bundle install
+  echo "Doing the database setup..."
+  bundle exec rake db:setup
+  echo
+  echo "Load default data for development environment..."
+bundle exec rake openfoodnetwork:dev:load_sample_data
 EOF
 echo "Databases ready!"
